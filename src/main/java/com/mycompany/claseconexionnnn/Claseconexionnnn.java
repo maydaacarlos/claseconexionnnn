d9576382a4fb467bf342b4538363f5e83114cba0
maydaacarlos <maydaacarlos@gmail.com>
2024-09-30 18:57:17 -0500

    carga inicial

---
+++ src/main/java/com/mycompany/claseconexionnnn/Claseconexionnnn.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.claseconexionnnn;
+
+/**
+ *
+ * @author mayda
+ */
+public class Claseconexionnnn {
+
+    public static void main(String[] args) {
+        conexion objetoconectar=new conexion();
+        objetoconectar.Conexionconbasededatos();
+        
+                
+        }
+}
---
+++ src/main/java/com/mycompany/claseconexionnnn/conexion.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.claseconexionnnn;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author mayda
+ */
+public class conexion {
+    Connection conectar=null;
+    String url="jdbc:postgresql://localhost:5432/POO";
+    String user= "postgres";
+    String password="123456";
+    public Connection Conexionconbasededatos(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            conectar=DriverManager.getConnection(url,user,password);
+            JOptionPane.showMessageDialog(null,"SE COENCTO CORRECTAMENTE ALA BASE DE DATOS");
+      
+        }catch (Exception e){
+            JOptionPane.showConfirmDialog(null,"ERROR AL CONECTARSE A LA BASE DE DATOS"+e.toString());
+        }
+        return conectar;
+    }
+    
+}
